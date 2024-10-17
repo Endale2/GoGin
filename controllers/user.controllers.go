94d1a6b8293bea84c4a726864706f99d72170fbd
Endale2 <endale406@gmail.com>
2024-10-17 05:22:57 +0300

    updated

--- controllers/user.controllers.go
+++ controllers/user.controllers.go
@@ -1,6 +1,10 @@
 package controllers
 
-import "go-gin/services"
+import (
+	"go-gin/services"
+
+	"github.com/gin-gonic/gin"
+)
 
 type UserController struct {
 	UserService services.UserService
@@ -10,4 +14,33 @@ func New(userService services.UserService) *UserController {
 	return &UserController{
 		UserService: userService,
 	}
+}
+
+func (uc *UserController) CreateUser(ctx *gin.Context) {
+	ctx.JSON(200, "success")
+}
+
+func (uc *UserController) GetUser(ctx *gin.Context) {
+	ctx.JSON(200, "success")
+}
+func (uc *UserController) GetAllUser(ctx *gin.Context) {
+	ctx.JSON(200, "success")
+}
+
+func (uc *UserController) UpdateUser(ctx *gin.Context) {
+	ctx.JSON(200, "success")
+}
+
+func (uc *UserController) DeleteUser(ctx *gin.Context) {
+	ctx.JSON(200, "success")
+}
+
+func (uc *UserController) RegisterRoutesGroup(rg *gin.RouterGroup) {
+	userroute := rg.Group("/user")
+	userroute.POST("/create", uc.CreateUser)
+	userroute.GET("/:name", uc.GetUser)
+	userroute.GET("/all_users", uc.GetAllUser)
+	userroute.PATCH("/update", uc.UpdateUser)
+	userroute.DELETE("/delete", uc.DeleteUser)
+
 }
