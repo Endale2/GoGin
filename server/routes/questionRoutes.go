0d6953ce0b5725bf1131b09b543e8e116f599c39
Endale2 <endale406@gmail.com>
2024-10-26 11:50:03 +0300

    updated

--- server/routes/questionRoutes.go
+++ server/routes/questionRoutes.go
@@ -2,12 +2,14 @@ package routes
 
 import (
 	"go-gin/controllers"
+	"go-gin/middleware"
 
 	"github.com/gin-gonic/gin"
 )
 
 func QuestionRoutes(r *gin.Engine) {
 	questionRoutes := r.Group("/questions")
+	questionRoutes.Use(middleware.AuthMiddleware())
 	{
 		questionRoutes.GET("/", controllers.GetQuestions)
 		questionRoutes.GET("/:id", controllers.GetQuestionByID)
